9d44dc449ee26cc294c8264ee20586820cce6ea0
carter-riley <carter.j.riley@outlook.com>
2018-01-24 15:59:26 -0800

    Wesley Change
    Programatic billiboards now added

--- Timeline Prototype/Assets/AddNewBillboards.cs
+++ Timeline Prototype/Assets/AddNewBillboards.cs
@@ -6,20 +6,36 @@ public class AddNewBillboards : MonoBehaviour {
 
 	public GameObject prefab;
 	public int numberOfObjects = 20;
-	//public float radius = 5f;
+    //public float radius = 5f;
+
+    public List<GameObject> billboardsList = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-		//Billoard clone = (Billoard)Instantiate(projectile, transform.position, transform.rotation);
-		//		var gameObject = new GameObject("Billboard");
-		//		var meshFilter = gameObject.AddComponent<MeshFilter>();
-		//		gameObject.AddComponent<MeshRenderer>();
-		for (int i = 0; i < numberOfObjects; i++) {
-			//float angle = i * Mathf.PI * 2 / numberOfObjects;
-			Vector3 pos = new Vector3(i-(i*80), 0, Random.Range(-60.0f, 60.0f));
-			Instantiate(prefab, pos, Quaternion.identity);
-		}
-		//
+        bool left = true;
+        float xPosition;
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            if (left)
+            {
+                xPosition = -25f;
+                left = false;
+            }
+            else
+            {
+                xPosition = 125f;
+                left = true;
+            }
+
+
+            Vector3 pos = new Vector3(i - (i * 200), 0, xPosition);
+            GameObject newBillboard = Instantiate(prefab, pos, Quaternion.identity);
+            newBillboard.transform.localScale = new Vector3(0.1F, 0.1F, 0.1F);
+            newBillboard.GetComponent<BillboardMonobehaviorFunctions>().boardNumber = billboardsList.Count;
+
+
+            billboardsList.Add(newBillboard);
+        }
 
 	}
 
---
+++ Timeline Prototype/Assets/BillboardMonobehaviorFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardMonobehaviorFunctions : MonoBehaviour {
+
+
+    public int boardNumber;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        PlayerPrefs.SetInt("billboardNumber", this.gameObject.GetComponent<BillboardMonobehaviorFunctions>().boardNumber);
+
+        Application.LoadLevel("EventView");
+    }
+}
