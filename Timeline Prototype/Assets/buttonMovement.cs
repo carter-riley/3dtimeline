0615107c2cc85826ce84e45d247b9b4971ff6349
opatera <31523899+opatera@users.noreply.github.com>
2018-01-31 18:14:14 -0800

    Buttons Added

---
+++ Timeline Prototype/Assets/buttonMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonMovement : MonoBehaviour
+{
+
+
+    public void moveCameraForward()
+    {
+        Camera.main.transform.Translate(new Vector3(20, 0, 0));
+    }
+    public void moveBackwards()
+    {
+        //Camera.main.transform.Translate(-10, 0, );
+    }/*
+    public void moveLeft()
+    {
+
+    }
+    public void moveRight()
+    {
+
+    }*/
+}
