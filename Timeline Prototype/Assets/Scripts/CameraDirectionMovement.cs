c95a4762a920924db67663cdcae1012f52b87096
opatera <31523899+opatera@users.noreply.github.com>
2018-03-28 16:38:24 -0700

    TouchScreen works
    Flipped Y axis

--- Timeline Prototype/Assets/Scripts/CameraDirectionMovement.cs
+++ Timeline Prototype/Assets/Scripts/CameraDirectionMovement.cs
@@ -13,12 +13,25 @@ public class CameraDirectionMovement : MonoBehaviour
     private float yaw = -90.0f;
     private float pitch = 0.0f;
 
-    private bool isRotating;	// Is the camera being rotated?
-    private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
+    private bool isRotating; // Is the camera being rotated?
+    //private Vector3 mouseOrigin; // Position of cursor when mouse dragging starts
     public float turnSpeed = 4.0f;      // Speed of camera turning when mouse moves in along an axis
     public float panSpeed = 4.0f;       // Speed of the camera when being panned
-    public float zoomSpeed = 4.0f;		// Speed of the camera going back and forth
+    public float zoomSpeed = 4.0f; // Speed of the camera going back and forth
 
+    Vector3 FirstPoint;
+    Vector3 SecondPoint;
+    float xAngle;
+    float yAngle;
+    float xAngleTemp;
+    float yAngleTemp;
+
+    void Start()
+    {
+        xAngle = 0;
+        yAngle = 0;
+        this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0);
+    }
 
     // Update is called once per frame
     void Update()
@@ -34,31 +47,85 @@ public class CameraDirectionMovement : MonoBehaviour
 
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);  */
 
-  
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    // Get mouse origin
+        //    mouseOrigin = Input.mousePosition;
+        //    isRotating = true;
+        //}
 
-        if (Input.GetMouseButtonDown(1))
+        //if (!Input.GetMouseButton(1)) isRotating = false;
+
+        //if (isRotating)
+        //{
+        //    Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+
+        //    bool inBottom = Camera.main.transform.eulerAngles.x <= 30 && Camera.main.transform.eulerAngles.x >= 0;
+        //    bool inTop = Camera.main.transform.eulerAngles.x >= 315 && Camera.main.transform.eulerAngles.x <= 360;
+        //    if ((Camera.main.transform.eulerAngles.x > 325 && inTop || pos.y < 0 && inTop) || (Camera.main.transform.eulerAngles.x < 23 && inBottom || pos.y > 0 && inBottom)) { //top bound
+
+        //        transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+        //        transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+
+        //    }
+        //}
+        //Source: https://answers.unity.com/questions/805630/how-can-%C4%B1-rotate-camera-with-touch.html
+        if (Input.touchCount > 0)
         {
-            // Get mouse origin
-            mouseOrigin = Input.mousePosition;
-            isRotating = true;
-        }
-
-        if (!Input.GetMouseButton(1)) isRotating = false;
-
-        if (isRotating)
-        {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-            
-            
-            bool inBottom = Camera.main.transform.eulerAngles.x <= 30 && Camera.main.transform.eulerAngles.x >= 0;
-            bool inTop = Camera.main.transform.eulerAngles.x >= 315 && Camera.main.transform.eulerAngles.x <= 360;
-            if ((Camera.main.transform.eulerAngles.x > 325 && inTop || pos.y < 0 && inTop) || (Camera.main.transform.eulerAngles.x < 23 && inBottom || pos.y > 0 && inBottom)) { //top bound
-                                
-                transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
-                transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
-                                    
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                FirstPoint = Input.GetTouch(0).position;
+                xAngleTemp = xAngle;
+                yAngleTemp = yAngle;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                SecondPoint = Input.GetTouch(0).position;
+                xAngle = xAngleTemp + (SecondPoint.x - FirstPoint.x) * 180 / Screen.width;
+                yAngle = yAngleTemp + (SecondPoint.y - FirstPoint.y) * -90 / Screen.height;
+                yAngle = Mathf.Clamp(yAngle, -20, 35);
+                this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
             }
         }
+        //Vector3 pos = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position - TouchPhase.Began);
+        //bool inBottom = Camera.main.transform.eulerAngles.x <= 30 && Camera.main.transform.eulerAngles.x >= 0;
+        //bool inTop = Camera.main.transform.eulerAngles.x >= 315 && Camera.main.transform.eulerAngles.x <= 360;
+        //if ((Camera.main.transform.eulerAngles.x > 325 && inTop || pos.y < 0 && inTop) || (Camera.main.transform.eulerAngles.x < 23 && inBottom || pos.y > 0 && inBottom)) { //top bound
+
+        //    transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+        //    transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+
+        //    }
+
+
+
     }
 }
 
+/* Code for touch controls
+function Update()
+{
+    //Check count touches
+    if (Input.touchCount > 0)
+    {
+        //Touch began, save position
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            firstpoint = Input.GetTouch(0).position;
+            xAngTemp = xAngle;
+            yAngTemp = yAngle;
+        }
+        //Move finger by screen
+        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            secondpoint = Input.GetTouch(0).position;
+            //Mainly, about rotate camera. For example, for Screen.width rotate on 180 degree
+            xAngle = xAngTemp + (secondpoint.x - firstpoint.x) * 180.0 / Screen.width;
+            yAngle = yAngTemp - (secondpoint.y - firstpoint.y) * 90.0 / Screen.height;
+            //Rotate camera
+            this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0);
+        }
+    }
+}
+*/
